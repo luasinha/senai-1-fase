d11a4dd7932ce8046236264ddb04c719cbe2f737
luanna <luannamuller1981@gmail.com>
2026-04-22 19:52:04 -0300

    Add files via upload

---
+++ 01 javascript/testes.js
@@ -0,0 +1,20 @@
+//alert("Ola mundo")
+//alert("segunda mensagem")
+// let nome
+
+let nome
+
+nome = "Luanna"
+let idade = 19
+
+//alert("idade: " + idade)
+//alert("nome: " + nome + "\nidade: " + idade)
+
+//nome = prompt("digite seu nome: ")
+//alert("Nome: " + nome)
+
+let nota1
+nota1 = prompt("digite a primeira nota: ")
+nota1 = number(nota1)
+nota1= nota1 + 1
+alert(nota1)
