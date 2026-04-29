1218ecbc15c1dcdadc0838f999d7998cbff9627f
LUANNA NUNES MÜLLER <luanna_muller@estudante.sesisenai.org.br>
2026-04-28 22:17:58 -0300

    organizandoooo

--- 01 javascript/testes.js
+++ 01 javascript/testes.js
@@ -13,15 +13,15 @@
 //alert("idade: " + idade)
 //alert("nome: " + nome + "\nidade: " + idade)
 
-//nome = prompt("digite seu nome: ")
-//alert("Nome: " + nome)
+// nome = prompt("digite seu nome: ")
+// alert("Nome: " + nome)
 
 // -------------------------------------
 
-//let nota1
-//nota1 = prompt("digite a primeira nota: ")
-//nota1 = number(nota1)
-//nota1= nota1 + 1
-//alert(nota1)
+// let nota1
+// nota1 = prompt("digite a primeira nota: ")
+// nota1 = number(nota1)
+// nota1= nota1 + 1
+// alert(nota1)
 
 // -------------------------------------
