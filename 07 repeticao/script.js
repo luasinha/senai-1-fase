7154c27dcc505dd74a3181bce84b3cb0e814631d
LUANNA NUNES MÜLLER <luanna_muller@estudante.sesisenai.org.br>
2026-06-23 21:32:40 -0300

    for

--- 07 repeticao/script.js
+++ 07 repeticao/script.js
@@ -1,101 +1,112 @@
-function exercicio1() {
-let soma = 0;
+function exercicio1(){
+    let soma = 0
 
-for (let i = 1; i <= 10; i++) {soma += i;
-}
-console.log(soma);
-}
+    for(let i = 1; i <= 10; i++){
+        soma += i
+    }
 
-function exercicio2() {
-for (let i = 1; i <= 10; i++) {
-console.log(`5 x ${i} = ${5 * i}`);
-}
+    console.log(soma)
 }
 
-function exercicio3() {
-for (let i = 1; i <= 10; i++) {
-console.log(i * i);
-}
-}
-function exercicio4() {
-let palavra = "JavaScript";
-for (let i = 0; i < palavra.length; i++) {
-console.log(palavra[i]);
-}
+function exercicio2(){
+    for(let i = 1; i <= 10; i++){
+        console.log("5 x " + i + " = " + (5 * i))
+    }
 }
 
-function exercicio5() {
-for (let i = 1; i <= 30; i++) {
-if (i % 3 === 0) {console.log(i)}
-}
-}
-
-function exercicio6() {
-let resultado = "";
-for (let i = 1; i <= 10; i++) {
-resultado += "*";
-}
-console.log(resultado);
+function exercicio3(){
+    for(let i = 1; i <= 10; i++){
+        console.log(i * i)
+    }
 }
 
-function exercicio7() {
-let soma = 0;
-for (let i = 2; i <= 20; i += 2) {
-soma += i;
-}
-console.log(soma);
+function exercicio4(){
+    let js = "javascript"
+
+    for(let i = 0; i < js.length; i++){
+        console.log(js[i])
+    }
 }
 
-function exercicio8() {
-for (let i = 0; i <= 50; i += 5) {
-console.log(i);
-}
-}
-
-function exercicio9() {
-for (let i = 1; i <= 8; i++) {
-console.log("Olá!");
-}
+function exercicio5(){
+    for(let i = 1; i <= 30; i++){
+        if(i % 3 == 0){
+            console.log(i)
+        }
+    }
 }
 
-function exercicio10() {
-let soma = 0;
-for (let i = 1; i <= 15; i += 2) {
-soma += i;
-}
-console.log(soma);
+function exercicio6(){
+    let asterisco = ""
+
+    for(let i = 1; i <= 10; i++){
+        asterisco += "*"
+    }
+
+    console.log(asterisco)
 }
 
-function exercicio11() {
-let soma = 0;
-for (let i = 1; i <= 15; i += 2) {
-soma += i;
-}
-console.log(soma);
+function exercicio7(){
+    let soma = 0
+
+    for(let i = 0; i <= 20; i += 2){
+        soma += i
+    }
+
+    console.log(soma)
 }
 
-function exercicio12() {
-let texto = "";
-for (let i = 1; i <= 5; i++) {
-texto += i;
-console.log(texto);
-}
+function exercicio8(){
+    for(let i = 0; i <= 50; i += 5){
+        console.log(i)
+    }
 }
 
-function exercicio13() {
-let fatorial = 1;
-for (let i = 1; i <= 5; i++) {
-fatorial *= i;
-}
-console.log(fatorial);
+function exercicio9(){
+    for(let i = 1; i <= 8; i++){
+        console.log("ola")
+    }
 }
 
-function exercicio14() {
-let palavra = prompt("Digite uma palavra:");
-let contador = 0;
+function exercicio10(){
+    let soma = 0
 
-for (let i = 0; i < palavra.length; i++) {
-if (palavra[i].toLowerCase() === "o") {contador++;}
+    for(let i = 1; i <= 15; i++){
+        if(i % 2 != 0){
+            soma += i
+        }
+    }
+
+    console.log(soma)
 }
-console.log("Quantidade de letras 'o':", contador);
+
+function exercicio11(){
+    let numeros = ["1", "12", "123", "1234", "12345"]
+
+    for(let i = 0; i < numeros.length; i++){
+        console.log(numeros[i])
+    }
+}
+
+function exercicio12(){
+    let resposta = 1
+
+    for(let i = 1; i <= 5; i++){
+        resposta *= i
+    }
+
+    console.log(resposta)
+}
+
+function exercicio13(){
+    let palavra = "ovo"
+    let contador = 0
+
+    for(let i = 0; i < palavra.length; i++){
+        if(palavra[i] == "o"){
+            contador++
+        }
+    }
+
+    console.log(contador)
 }
