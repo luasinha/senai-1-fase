55d92ffd5ceb8f1ea0e6d3f7496c6188f8eb456f
LUANNA NUNES MÜLLER <luanna_muller@estudante.sesisenai.org.br>
2026-07-07 19:52:01 -0300

    organizando

--- 010 array/script.js
+++ 010 array/script.js
@@ -1,3 +1,39 @@
+function contador(){
+
+    let i = 0
+    while(i < 10){
+    i++
+        
+    console.log(i)
+}
+}
+    
+function numeros_pares(){
+    for(let i = 0; i <= 20; i++){
+    if(i%2 === 0){
+    
+    console.log(i)
+    }
+}
+
+}
+
+function arrays(){
+    let idades = [12, 3, 5, 48, 91, 32]
+
+    array.pop() // remove o ultimo array
+    array.splice(2, 1)// remove o array com os números da posição e quantidade respectivamente
+    array.push()//adiciona dentro do array
+    idades.length // quantidade de elementos dentro do array
+
+    for(let i = 0; i < idades.length; i++)
+    ===
+    for(let i = 0; i < 6; i++)
+}
+
+
+
+
 let idades = []
 
 function maisiIdades(){
