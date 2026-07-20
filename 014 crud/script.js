977343e927a8650524b597cb671513df327f3553
LUANNA NUNES MÜLLER <luanna_muller@estudante.sesisenai.org.br>
2026-07-20 19:30:45 -0300

    crud

--- 014 crud/script.js
+++ 014 crud/script.js
@@ -1,67 +1,145 @@
-// C >> create >> cadastrar
 
-// R >> read >> ler
+// C >> Create  >> Cadastrar
+// R   >> Read   >> Ler
+// U   >>  Update  >> alterar/atualizar/editar/fuçar/mudar
+// D   >>  Delete  >> Apagar/deletar/excluir
 
-// U >> update >> alterar/atualizar/editar/mudar
-
-// D >> delete >> apagar
-
-// ======================================================
+// ctrl + ;
 
 // const nomes = []
 // const alturas = []
 
-// let dino = {
-//     nome: "velocirraptor", 
-//     altura: 3,
-//     cor: "marrom",
-//     custo: 14
-// }
+
+
 
 // console.log(dino);
 
-const dinos = []
+let dinos = []
 
-function cadastrarDino(){
-    const novoDino = {
-        id: Date.now(),
-        nome: document.getElementById('input-nome').value,
-        altura: Number(document.getElementById('input-altura').value),
-        cor: document.getElementById('input-cor').value,
-        custo: Number(document.getElementById('input-custo').value),
-    }
-
+function cadastrarDino() {
+const novoDino = {
+id: Date.now(),
+nome: document.getElementById('input-nome').value,
+altura: Number(document.getElementById("input-altura").value),
+cor: document.getElementById("input-cor").value,
+custo: Number(document.getElementById("input-custo").value),
+}
 dinos.push(novoDino)
 
 console.log(dinos);
 
 limparFormulario()
+mostrarTodos()
 }
 
-
-function limparFormulario(){
-
+function limparFormulario() {
 document.getElementById('input-nome').value = ''
 document.getElementById('input-altura').value = ''
 document.getElementById('input-cor').value = ''
-document.getElementById('input-custo').value= ''
+document.getElementById('input-custo').value = ''
+document.getElementById('input-id').value = ''
 
 document.getElementById('input-nome').focus()
-document.getElementById('input-altura').focus()
-document.getElementById('input-cor').focus()
-document.getElementById('input-custo').focus()
 }
 
+function mostrarTodos(){
+document.getElementById('painel-dinos').innerHTML = '' 
 
-function testar(){
-    const novoDino = {
-       nome: prompt('nome'), 
-       altura: 3,
-       cor: "marrom",
-       custo: 14
-    }
+for(let i=0; i<dinos.length; i++){
+// alert(dinos[i].nome)
+document.getElementById('painel-dinos').innerHTML += `<div class="card-dino">
+ <h2>${dinos[i].nome}</h2>
+ <p>Altura: ${dinos[i].altura}</p>
+ <p>Cor: ${dinos[i].cor}</p>
+ <p>Custo: ${dinos[i].custo}</p>
+ <p>${dinos[i].id}</p>
+    </div>` 
+}
+}
 
-dinos.push(novoDino)
+function testar() {
+dinos = [
+{
+id: 1718324500001,
+nome: "Rex",
+altura: 6.1,
+cor: "#8B4513", // Marrom
+custo: 150000
+},
+{
+id: 1718324500002,
+nome: "Velo",
+altura: 2.0,
+cor: "#2E8B57", // Verde escuro
+custo: 85000
+},
+{
+id: 1718324500003,
+nome: "Trike",
+altura: 3.0,
+cor: "#708090", // Cinza
+custo: 120000
+},
+{
+id: 1718324500004,
+nome: "Braqui",
+altura: 13.0,
+cor: "#556B2F", // Verde oliva
+custo: 250000
+},
+{
+id: 1718324500005,
+nome: "Ptero",
+altura: 1.5,
+cor: "#4682B4", // Azul aço
+custo: 95000
+}
+]
+console.log(dinos);
+}
 
-console.log(dinos)
+function pesquisar(){
+let nomeProcurado = document.getElementById('input-nome').value
+
+for(let i = 0; i<dinos.length; i++){
+if(nomeProcurado == dinos[i].nome ){
+console.log(dinos[i]);
+document.getElementById('input-altura').value = dinos[i].altura
+document.getElementById('input-cor').value = dinos[i].cor
+document.getElementById('input-custo').value = dinos[i].custo
+document.getElementById('input-id').value = dinos[i].id
+console.log(i);
+} 
+}
+}
+
+function salvarDino(){
+let id = Number(document.getElementById('input-id').value)
+
+for(let i = 0; i<dinos.length; i++){
+if(id == dinos[i].id ){
+console.log(dinos[i]);
+dinos[i].altura = document.getElementById('input-altura').value 
+dinos[i].cor = document.getElementById('input-cor').value 
+dinos[i].custo = document.getElementById('input-custo').value 
+dinos[i].id = document.getElementById('input-id').value 
+console.log(i);
+}
+}
+mostrarTodos()
+limparFormulario()
+}
+
+function excluirDino(){
+let id = Number(document.getElementById('input-id').value)
+
+for(let i = 0; i<dinos.length; i++){
+if(id == dinos[i].id ){
+    console.log(dinos[i]);
+    dinos.splice(i, 1)
+    console.log(i);
+}
+}
+mostrarTodos()
+limparFormulario()
 }
